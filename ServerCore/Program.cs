a19e938380cfaa49581c51aaa1e41690f69ce2fa
kimhyeok <kimmang940715@naver.com>
2023-11-20 03:45:15 +0900

    쓰레드 생성

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Threading;
 namespace ServerCore
 {
     class Program
     {
+        static void MainThread()
+        {
+            Console.WriteLine("Hello Thread!");
+        }
+
         static void Main(string[] args)
         {
+            Thread t = new Thread(MainThread);
+            t.Start();
+
+            Console.WriteLine("Hello World");
         }
     }
 }
